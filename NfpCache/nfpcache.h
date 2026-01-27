ff29fa759f257fdf61fe81fba6c7883e0bf980c3
fourroadsfloyd <92658784+fourroadsfloyd@users.noreply.github.com>
2026-01-27 20:24:25 +0800

    Update nfpcache.h

--- NfpCache/nfpcache.h
+++ NfpCache/nfpcache.h
@@ -19,11 +19,11 @@ struct Pair{
     Item* A = nullptr;
     Item* B = nullptr;
 
-    uint16_t rotateIndex;       //存储B的vector索引号,if isMirror==false,就是rotate_items中的索引，else 就是mirror_rotate_items中的索引
+    uint16_t rotateIndex; 
 
-    bool isMirror;              //B是镜像吗?
+    bool isMirror;              
 
-    bool AisSheet;              //A是板材吗?
+    bool AisSheet;             
 };
 
 using PairKey = uint64_t;
@@ -41,41 +41,36 @@ class NfpCache : public QObject{
 public:
     NfpCache() = delete;
 
-    NfpCache(Sheets &sheets_, Items &items_);       //获取板材和零件信息
+    NfpCache(Sheets &sheets_, Items &items_);     
 
     void run();
-
-    //返回的Paths,外边缘是逆时针，孔洞是顺时针，A,B都是旋转队列第1个，即原始件。
-    //B碰A
+   
     Paths readItemAndItemNfp(ItemStates& A_, ItemStates& B_, int rotate_A_index, int rotate_B_index, bool ismirror_A, bool ismirror_B);
 
-    //返回的Paths,外边缘是逆时针，孔洞是顺时针。需注意对于板材而言，逆时针的左侧有效，顺时针的右侧有效
-    //A 是sheet板
     Paths readSheetAndItemIfp(Item& A, Item& B, int rotate_B_index, bool ismirror_B);
 
 private:
     Paths getNfpfromCache(Pair &pair);
 
-    static void BuildFitPolygon(const Pair &pair);  //传入线程池，Pair的处理函数
+    static void BuildFitPolygon(const Pair &pair);  
 
     static PairKey getPairKey(const Pair& pair);
 
-    inline void MatchPair();                        //板材和零件配对，零件和零件配对，配对结果保存在m_match_pairs中，需注意，pair.A永远是面积最大的
+    inline void MatchPair();                        
     inline void CreatePair(Item &A, ItemStates &itemstates_B, bool AisSheet);
 
     inline bool Compare(Item &A, Item &B);
 
 private:
-    Sheets* m_sheets;//必须编号 Item.m_id,m_area必须提前计算好
-    Items* m_items;//必须编号 Item.m_id，m_area必须提前计算好
-
+    Sheets* m_sheets;
+    Items* m_items;
 
     std::vector<Pair> m_match_pairs;
 
     QFuture<void> m_future;
-    QFutureWatcher<void> m_watcher;  // 监听异步任务完成
+    QFutureWatcher<void> m_watcher; 
 
-    Paths m_result;                                 //外部read得到的NFP
+    Paths m_result;                          
 
 signals:
     void nfpBuildFinished();
